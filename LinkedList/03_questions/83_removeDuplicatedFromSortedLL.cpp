9ba5d3b481ce058a765562a34e455a8d0289383b
Bikash Shaw <vshaw138@gmail.com>
2026-07-01 10:54:39 +0530

    remove duplicate modes from a sorted linked list

---
+++ LinkedList/03_questions/83_removeDuplicatedFromSortedLL.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node() {
+      this->next = NULL;
+    }
+    Node(int data) {
+      this->data = data;
+      this->next = NULL;
+    }
+    void print(Node* head) {
+      Node* temp = head;
+      while(temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+      }
+      cout << endl;
+    }
+    ~Node(){
+      cout << "Destructor Called";
+    }
+};
+
+Node* headOfModifiedLL(Node* &head) {
+    //As in the line 35 we are using next of head
+    //So this dont forget to add this check
+    if(head == NULL){
+        return head;
+    }
+    Node*prev = head;
+    Node* curr = head->next;
+
+    while(curr != NULL) {
+        if(prev->data == curr->data) {
+            //delete curr node
+            Node* temp = curr;
+            prev->next = curr->next;
+            curr = curr->next;
+            delete temp;
+        } else {
+            prev = curr;
+            curr = curr->next;
+        } 
+    }
+    return head;
+}
