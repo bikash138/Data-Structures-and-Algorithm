1dc4b87d352bb784dac62786390af1e8c16b6a62
Bikash Shaw <vshaw138@gmail.com>
2026-08-14 12:14:40 +0530

    add more questions on basic recursion

--- Recusrion/01_basics.cpp
+++ Recusrion/01_basics.cpp
@@ -51,8 +51,61 @@ int maxElement(int *arr,int index, int size) {
     return max;
 }
 
+// FIND ODD ELEMENTS IN AN ARRAY
+void oddElement(int *arr, int index, int size) {
+    if(index >= size) return;
+    if(arr[index] & 1) {
+        cout << arr[index] << " ";
+    }
+    oddElement(arr, index+1, size);
+}
+
+// IMPLEMENT BINARY SEARCH USING RECURSION
+int recursiveBinarySearch(int *arr, int s, int e, int target) {
+    // If end crossed start means the target is not present
+    if(s > e) {
+        return -1;
+    }
+    // Get the initial mid element
+    int mid = (s+e)/2;
+    // If target found then simply return it
+    if(arr[mid] == target) {
+        return mid;
+    }
+    /*
+    If target is smaller than mid term then 
+    call the recursive function for the left half
+    after shifting the end pointer
+    */
+    if(target < arr[mid]) {
+        int recAns = recursiveBinarySearch(arr, s, mid-1, target);
+        return recAns;
+    } 
+    // Else right shift of the start pointer
+    else {
+        int recAns = recursiveBinarySearch(arr, mid+1, e, target);
+        return recAns;
+    }
+}
+
+void printDigitsUsingRecursion(int num) {
+    if(num == 0) return;
+    int digit = num % 10;
+    num = num / 10;
+    printDigitsUsingRecursion(num);
+    cout << digit << ' ';
+}
+
+bool isSortedArray(int *arr, int index, int size) {
+    if(index == size-1) return true;
+    if(arr[index] > arr[index+1]) {
+        return false;
+    }
+    return isSortedArray(arr, index+1, size);
+}
+
 int main() {
-    int arr[5] = {20, 10, 70, 40, 90};
+    int arr[5] = {20,310, 700, 4000, 9000};
     int size = 5;
 
     // printArray(arr, 5, 0);
@@ -61,4 +114,7 @@ int main() {
     cout << endl;
     // cout << search(arr, 0, 5, 10);
     // cout << minElementUsingTailRecursion(arr, 0, 5, INT_MAX);
-} 
+    // oddElement(arr, 0, 5);
+    // printDigitsUsingRecursion(3214);
+    cout << isSortedArray(arr, 0, size);
+}
